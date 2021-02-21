7546a30f1d57d8d262fd1512b4913a03917099e0
Luca Demian <lucadem1313@gmail.com>
2021-02-20 19:25:52 -0500

    working on plant list

--- lib/ui/PlantTile.dart
+++ lib/ui/PlantTile.dart
@@ -8,14 +8,30 @@ class PlantTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListTile(
-        leading: Image(image: AssetImage('assets/plants/' + ((this.plant.height / this.plant.maxHeight * 16).toInt()).toString() + '.png')),
-        title: Text(plant.name),
-        subtitle: Text(plant.createdAt.toString()),
-        onTap: () {
-          Navigator.of(context).pushNamed("/view-plant", arguments: plant);
-        },
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.black12, width: 1),
+          borderRadius: BorderRadius.all(Radius.circular(6)),
+        ),
+        child: ListTile(
+          leading: Image(image: AssetImage('assets/plants/' + ((this.plant.height / this.plant.maxHeight * 16).toInt()).toString() + '.png')),
+          title: Text(plant.name),
+          subtitle: Padding(
+            padding: EdgeInsets.only(top: 8.0),
+            child: Row(
+              children: [
+                Icon(Icons.opacity),
+                Text((plant.waterLevel ~/ plant.maxWater).toString() + "%"),
+              ],
+            ),
+          ),
+          onTap: () {
+            Navigator.of(context).pushNamed("/view-plant", arguments: plant);
+          },
+        ),
       ),
     );
   }
--- lib/ui/PlantsList.dart
+++ lib/ui/PlantsList.dart
@@ -17,15 +17,25 @@ class PlantsList extends StatelessWidget {
       itemCount: plants.length + 1,
       itemBuilder: (context, idx) {
         if (idx == 0) {
-          return Container(
-            width: MediaQuery.of(context).size.width - 100,
-            child: SolidButton(
-              "Grow a New Plant", 
-              theme: SolidButtonTheme.green,
-              onPressed: () => {
-                Navigator.of(context).pushNamed("/new-plant")
-              },
-            ),
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 50.0),
+                child: Text("My Plants", style: Theme.of(context).textTheme.headline1,),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 30, bottom: 20),
+                width: MediaQuery.of(context).size.width - 100,
+                child: SolidButton(
+                  "Grow a New Plant",
+                  theme: SolidButtonTheme.green,
+                  onPressed: () => {
+                    Navigator.of(context).pushNamed("/new-plant")
+                  },
+                ),
+              ),
+            ],
           );
         }
         Plant thisPlant = plants[idx - 1];
